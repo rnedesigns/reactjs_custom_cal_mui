08eee8f410b507beca9ef1bc428d98f9c4e7f01c
rnedesigns <rn021215@gmail.com>
2022-03-11 14:53:12 +0530

    updated for useEffect to fix data fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Calendar from './Calendar';
 import 'bulma/css/bulma.min.css';
+import CalendarTwo from './CalendarTwo';
 
 function App() {
   return (
     <div className="container" style={ { color: "white" } }>
       <Calendar />
+      {/* <CalendarTwo /> */ }
     </div>
   );
 }
--- src/CalendarTwo.js
+++ src/CalendarTwo.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import useCalendar from "react-use-calendar";
 // import "./fonts/font-awesome/css/font-awesome.min.css";
 
-function Calendar() {
+function CalendarTwo() {
+    const [data, setData] = useState([]);
+
     const [state, actions] = useCalendar(null, {
         events: [
             {
@@ -19,6 +22,17 @@ function Calendar() {
             }
         ]
     });
+
+    const getInfo = () => {
+        fetch('./data.json', { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } })
+            .then(res => res.json)
+            .then(setData)
+    }
+
+    useEffect(() => {
+        getInfo()
+    }, [])
+
     // console.log(actions.addEvent);
     return (
         <table className="container">
@@ -44,8 +58,9 @@ function Calendar() {
                         { week.map(day => (
                             <td key={ day.dayOfMonth } style={ { textAlign: "center", backgroundColor: day.isToday ? "#8c939f" : "#525d72", flexGrow: "1", padding: "3em", border: ".1em solid #282c34", width: '200px' } }>
                                 <b>{ day.dayOfMonth }</b>
-                                <br /><span style={ { fontSize: "12px" } }>Entered 10:30 am</span><br /><span style={ { fontSize: "12px" } }>Exit 3:30 pm</span>
-                                {/* { actions.addEvent.events.map((e) => (<span>Entered: { e.enteredAt } {e.exitAt}</span>)) } */ }
+                                <br />
+                                {/* <br /><span style={ { fontSize: "12px" } }>Entered 10:30 am</span><br /><span style={ { fontSize: "12px" } }>Exit 3:30 pm</span> */ }
+                                { data && data.length > 0 && data.map((e) => <span>{ e.enteredAt }</span>) }
                             </td>
                         )) }
                     </tr>
@@ -55,4 +70,4 @@ function Calendar() {
     );
 }
 
-export default Calendar;
+export default CalendarTwo;
